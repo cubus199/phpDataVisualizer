3798dfb833149522135410f35ce8487007193df6
Luca <luc4r4t0r@gmail.com>
2020-01-22 11:13:22 +0100

    new theme added

---
+++ themes/darkNature.php
@@ -0,0 +1,6 @@
+<?php
+$config=array(
+"primaryColors" => array('#ffd151','#a1c359','#50aa71','#008b7f','#116977','#2f4858'),
+"secondaryColors" => array('#ffd151','#a1c359','#50aa71','#008b7f','#116977','#2f4858')
+);
+?>
