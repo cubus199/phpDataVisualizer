4b468da9e5c5cd2445086f690a6835fc331b7eee
LUC4R4T0R <luc4r4t0r@gmail.com>
2020-08-03 11:56:17 +0200

    changed indentation

--- font.php
+++ font.php
@@ -1,10 +1,10 @@
 <?php
 class font{
-    public $path;
-    public $name;
-    function __construct($path, $name){
-        $this->path = $path;
-        $this->name = $name;
-    }
+	public $path;
+	public $name;
+	function __construct($path, $name){
+		$this->path = $path;
+		$this->name = $name;
+	}
 }
 ?>
