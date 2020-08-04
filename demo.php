f4ba3aaddb6b7f374f6d718f95300a20191ba075
LUC4R4T0R <luc4r4t0r@gmail.com>
2020-08-04 20:52:22 +0200

    changed a few bits and included imagick demo

--- demo.php
+++ demo.php
@@ -1,17 +1,26 @@
 <?php
 require_once 'svgDrawingAgent.php';
 require_once 'gdDrawingAgent.php';
+require_once 'imDrawingAgent.php';
 
-$das = array();
-$das[0] = new svgDrawingAgent(1280, 720, new color('#ffffff'));
-$das[1] = new gdDrawingAgent(1280, 720, new color('#ffffff'), PNG_BASE64_OUTPUT);
+$names = array(
+	'SVG',
+	'Imagick',
+	'GD'
+);
+
+$das = array(
+	new svgDrawingAgent(1280, 720, new color('#ffffff')),
+	new imDrawingAgent(1280, 720, new color('#ffffff'), PNG_BASE64_OUTPUT),
+	new gdDrawingAgent(1280, 720, new color('#ffffff'), PNG_BASE64_OUTPUT)
+);
 
 $out = array();
 
 $j = 0;
 foreach($das as $da){
 
-	$da->drawRectangle(10,10,1270,710,new color('#A6101050'));
+	$da->drawRectangle(10,10,1270,710,new color('#A6101050'), false);
 	$da->drawPolygon(array(640,200, 980,360, 640,520, 300,360),new color('#1010A650'), false, 20);
 	$da->drawLine(10,10,1270,710,4,new color('#A61010'), true);
 	$da->drawLine(10,710,1270,10,4,new color('#A61010'));
@@ -19,7 +28,6 @@ foreach($das as $da){
 	$da->drawArc(1080,120,80,270,0,new color('#10A61050'));
 	$da->drawArc(1080,600,80,0,90,new color('#10A61050'));
 	$da->drawArc(200,600,80,90,180,new color('#10A61050'));
-
 	$points = array();
 	for($i = 0; $i <= 100; $i++){
 		array_push($points, 400 + $i * 480 / 100);
@@ -28,7 +36,8 @@ foreach($das as $da){
 
 	$da->drawPolyLine($points, 2, new color('#6010A0'), true);
 
-	$da->drawText(640, 360, 'Test', new font('FreeSans.ttf', 'FreeSans'), 80, new color('#000000'), CENTER, CENTER);
+	$da->drawText(640, 360, 'Test', new font('FreeSans.ttf', 'FreeSans'), 80, new color('#000000'), CENTER, CENTER, 90);
+	$da->drawText(40, 20, $names[$j], new font('FreeSans.ttf', 'FreeSans'), 50, new color('#000000'), LEFT, TOP);
 
 	$out[$j] = $da->finish();
 
@@ -37,4 +46,6 @@ foreach($das as $da){
 
 print_r($out[0]);
 echo '<img src="data:image/png;base64,'.$out[1].'" width="100%" height="auto">';
+echo '<img src="data:image/png;base64,'.$out[2].'" width="100%" height="auto">';
+
 ?>
