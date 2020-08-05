c2f4fc90a39a2a8e075abc5e56708c058955e15a
Adrian <adrian@baehr-bonn.de>
2020-08-05 16:14:23 +0200

    changed spaces into tab

--- graphHelper/GraphScaling.php
+++ graphHelper/GraphScaling.php
@@ -1,39 +1,39 @@
 <?php
 class GraphScaling{
-    public int $graphX1;//X coordinate of the upper left corner
-    public int $graphY1;//Y coordinate of the upper left corner
-    public int $graphX2;//X coordinate of the bottom right corner
-    public int $graphY2;//Y coordinate of the bottom right corner
-    public float $maxX;// scaling in X direction
-    public float $maxY;// scaling in Y direction
-    public float $minX;// scaling in X direction
-    public float $minY;// scaling in Y direction
+	public int $graphX1;//X coordinate of the upper left corner
+	public int $graphY1;//Y coordinate of the upper left corner
+	public int $graphX2;//X coordinate of the bottom right corner
+	public int $graphY2;//Y coordinate of the bottom right corner
+	public float $maxX;// scaling in X direction
+	public float $maxY;// scaling in Y direction
+	public float $minX;// scaling in X direction
+	public float $minY;// scaling in Y direction
 
-    public function __construct(int $x1, int $y1, int $x2, int $y2, float $maxX, float $maxY, float $minX = 0, float $minY = 0){
-        $this->graphX1 = $x1;
-        $this->graphY1 = $y1;
-        $this->graphX2 = $x2;
-        $this->graphY2 = $y2;
-        $this->maxX = $maxX;
-        $this->maxY = $maxY;
-        $this->minX = $minX;
-        $this->minY = $minY;
-    }
+	public function __construct(int $x1, int $y1, int $x2, int $y2, float $maxX, float $maxY, float $minX = 0, float $minY = 0){
+		$this->graphX1 = $x1;
+		$this->graphY1 = $y1;
+		$this->graphX2 = $x2;
+		$this->graphY2 = $y2;
+		$this->maxX = $maxX;
+		$this->maxY = $maxY;
+		$this->minX = $minX;
+		$this->minY = $minY;
+	}
 
-    /**
-     * calculates the X scaling according to the given limits
-     */
-    public function calcScaleX(){
-        $height = $x2 - $x1;
-        return $height / ($maxX - $minX);
-    }
+	/**
+	 * calculates the X scaling according to the given limits
+	 */
+	public function calcScaleX(){
+		$height = $x2 - $x1;
+		return $height / ($maxX - $minX);
+	}
 
-    /**
-     * calculates the y scaling according to the given limits
-     */
-    public function calcScaleY(){
-        $width = $y2 - $y1;
-        return $width / ($maxY - $minY);
-    }
+	/**
+	 * calculates the y scaling according to the given limits
+	 */
+	public function calcScaleY(){
+		$width = $y2 - $y1;
+		return $width / ($maxY - $minY);
+	}
 }
 ?>
